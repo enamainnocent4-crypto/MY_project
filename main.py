b45e265ca011a2cab1533b2f22ffe66d314e00e5
Enama <enamainnocent4@gmail.com>
2025-12-29 23:31:07 +0100

    main.py

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ def extract_products():
 
 extract_products()
 # #transformation des produits
-# def transform_produts():
-#     df = df.copy()
-#     cols_drop = ["images", "thumbnail", "description"]
-#     df.drop(columns = cols_drop, inplace = True, axis = 1)
-#     df.rename(columns={
-#         "title": "product_name",
-#         "price": "price_usd",
-#         "discountPercentage": "discount_pct"
-#     }, inplace=True)
+def transform_produts():
+    df = df.copy()
+    cols_drop = ["images", "thumbnail", "description"]
+    df.drop(columns = cols_drop, inplace = True, axis = 1)
+    df.rename(columns={
+        "title": "product_name",
+        "price": "price_usd",
+        "discountPercentage": "discount_pct"
+    }, inplace=True)
     
-#     df["final_price"] = df["price_usd"] * (1 - df["discount_pct"] / 100)
+    df["final_price"] = df["price_usd"] * (1 - df["discount_pct"] / 100)
     
-#     df["final_price"] = df["final_price"].round(2)
-#     df["rating"] = df["rating"].astype(float)
-#     df["stock"] = df["stock"].astype(int)
+    df["final_price"] = df["final_price"].round(2)
+    df["rating"] = df["rating"].astype(float)
+    df["stock"] = df["stock"].astype(int)
 
-#     df.fillna({
-#         "brand": "Unknown",
-#         "category": "Unknown"
-#     }, inplace=True)
+    df.fillna({
+        "brand": "Unknown",
+        "category": "Unknown"
+    }, inplace=True)
