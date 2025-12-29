25732562a81e2190bb340f95707b6cf990b34b5e
Enama <enamainnocent4@gmail.com>
2025-12-29 23:59:30 +0100

    main.py

--- main.py
+++ main.py
@@ -11,26 +11,4 @@ def extract_products():
         return df
     else:
         raise Exception("Erreur lors de l'extraction des données dans l'API")
-
 extract_products()
-
-def transform_produts():
-    df = df.copy()
-    cols_drop = ["images", "thumbnail", "description"]
-    df.drop(columns = cols_drop, inplace = True, axis = 1)
-    df.rename(columns={
-        "title": "product_name",
-        "price": "price_usd",
-        "discountPercentage": "discount_pct"
-    }, inplace=True)
-    
-    df["final_price"] = df["price_usd"] * (1 - df["discount_pct"] / 100)
-    
-    df["final_price"] = df["final_price"].round(2)
-    df["rating"] = df["rating"].astype(float)
-    df["stock"] = df["stock"].astype(int)
-
-    df.fillna({
-        "brand": "Unknown",
-        "category": "Unknown"
-    }, inplace=True)
