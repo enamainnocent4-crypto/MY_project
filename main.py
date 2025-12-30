839b4e96689e02d5d69ae9be891a0c028dec84f4
Enama <enamainnocent4@gmail.com>
2025-12-30 08:43:34 +0100

    main.py

--- main.py
+++ main.py
@@ -12,3 +12,31 @@ def extract_products():
     else:
         raise Exception("Erreur lors de l'extraction des données dans l'API")
 extract_products()
+
+def transform_products(df):
+    df = df.copy()
+    cols_drop = ["images", "thumbnail", "description"]
+    df.drop(columns = cols_drop, inplace=True, axis=1, errors='ignore')
+    df.rename(columns = { 
+        "title": "product_name",
+        "price": "price_usd",
+        "discountPercentage": "discount_pct"
+    }, inplace=True)
+    
+    df["final_price"] = df["price_usd"] * (1 - df["discount_pct"] / 100)
+    df["final_price"] = df["final_price"].round(2)
+    df["rating"] = df["rating"].astype(float)
+    df["stock"] = df["stock"].astype(int)
+
+    df.fillna({
+        "brand": "Unknown",
+        "category": "Unknown"
+    }, inplace=True)
+    
+    df["final_price"] = df["final_price"].apply(lambda x: f"${x}")
+    df["discount_pct"] = df["discount_pct"].apply(lambda x: f"{x}%")
+
+    return df
+df_raw = extract_products()
+df_clean = transform_products(df_raw)
+print(df_clean.head())
